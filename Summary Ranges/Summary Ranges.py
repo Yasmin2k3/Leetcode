283ea0cb57bf66ac067f650d750a43af8da5fe97
Yasmin <yasminwoodlock@icloud.com>
2023-06-12 21:27:06 +0100

    Added testing

--- Summary Ranges/Summary Ranges.py
+++ Summary Ranges/Summary Ranges.py
@@ -1,6 +1,21 @@
 class Solution(object):
     def summaryRanges(self, nums):
+        to = "->"
+        # temp = str(nums[0])
+
+        # loop through numbers in nums
+
+        # if nums[1] != nums[0]:
+        # nums[0] if nums[0] == nums[1]
+        # always add nums at start.
+
+        for i in nums:
+            print(i)
         """
         :type nums: List[int]
         :rtype: List[str]
         """
+
+
+testList = [0, 1, 2, 4, 5, 6, 7, 9]
+Solution.summaryRanges(Solution, testList)
