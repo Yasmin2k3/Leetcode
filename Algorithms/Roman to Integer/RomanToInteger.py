30e6b19fb7bea53bd7155bebe3bd145a70831d2f
Yasmin <yasminwoodlock@icloud.com>
2023-06-17 13:02:00 +0100

    made it smaller

--- Algorithms/Roman to Integer/RomanToInteger.py
+++ Algorithms/Roman to Integer/RomanToInteger.py
@@ -1,15 +1,7 @@
 class Solution(object):
-    def romanToInt(self, s):
+    def romanToInt(self, s: str) -> int:
         finalNumber = 0
-        romanKey = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-        }
+        romanKey = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
         for letterPos in range(len(s)-1):
             currentLetter = romanKey[s[letterPos]]
@@ -20,6 +12,4 @@ class Solution(object):
             else:
                 finalNumber += currentLetter
 
-        finalNumber += romanKey[s[-1]]
-
-        return finalNumber
+        return finalNumber + romanKey[s[-1]]
