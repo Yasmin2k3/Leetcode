1d3bdaed9471ea94b675f0693c3ea986a7bb03e9
Yasmin <yasminwoodlock@icloud.com>
2023-06-17 12:58:02 +0100

    Integer Problem works

--- Algorithms/Roman to Integer/RomanToInteger.py
+++ Algorithms/Roman to Integer/RomanToInteger.py
@@ -1,6 +1,25 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+        finalNumber = 0
+        romanKey = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        for letterPos in range(len(s)-1):
+            currentLetter = romanKey[s[letterPos]]
+            nextLetter = romanKey[s[letterPos + 1]]
+
+            if nextLetter > currentLetter:
+                finalNumber -= currentLetter
+            else:
+                finalNumber += currentLetter
+
+        finalNumber += romanKey[s[-1]]
+
+        return finalNumber
