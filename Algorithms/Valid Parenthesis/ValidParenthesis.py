eafca42376cec8b8352a187cdac0edf0e33a634c
Yasmin <yasminwoodlock@icloud.com>
2023-06-21 16:29:00 +0100

    Finished

--- Algorithms/Valid Parenthesis/ValidParenthesis.py
+++ Algorithms/Valid Parenthesis/ValidParenthesis.py
@@ -6,7 +6,20 @@ Every close bracket has a corresponding open bracket of the same type."""
 
 class Solution(object):
     def isValid(self, s: str) -> bool:
-        """
-        :type s: str
-        :rtype: bool
-        """
+        li = []
+        closeToOpen = {")": "(", "]": "[", "}": "{"}
+
+        for p in s:
+            if p in closeToOpen:
+                if li and li[-1] == closeToOpen[p]:
+                    li.pop()
+                else:
+                    return False
+            else:
+                li.append(p)
+
+        if not li:
+            return True
+        else:
+            return False
+
