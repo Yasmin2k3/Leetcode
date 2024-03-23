c48f054e38092b9b047ff09ece1164065c343dce
Yasmin <yasminwoodlock@icloud.com>
2024-03-23 23:56:16 +0000

    Made speed of Two Sum much better

--- Algorithms/Two Sum/Two Sum.py
+++ Algorithms/Two Sum/Two Sum.py
@@ -1,33 +1,10 @@
 class Solution(object):
-    nums = [3,3]
-    target = 6
+    def twoSum(nums, target):
+        h = []
+        for n in range(len(nums)):
+            for c in range(n+1, len(nums)):
+                if target - nums[n] == nums[c]:
+                    h.append(n)
+                    h.append(c)
 
-    def twoSum(self, nums, target):
-        finalSum = 0
-
-        for i in range(len(nums)):
-
-            for a in range(i, len(nums)):
-                if nums[a] == target:
-                    return "This number does not work"
-                elif nums[a] < target:
-                    finalSum += nums[a]
-                    break
-
-            for checker in range(len(nums)):
-                if checker == a:
-                    pass
-                elif nums[checker] == target:
-                    return "This number does not work"
-                elif nums[checker] + finalSum == target:
-                    finalSum += nums[checker]
-                    break
-
-            if finalSum == target:
-                break
-            elif nums[i] != nums[-1]:
-                finalSum = 0
-            else:
-                return "This number does not work"
-
-        return finalSum
+        return h
